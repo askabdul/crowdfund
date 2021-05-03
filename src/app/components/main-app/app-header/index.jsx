a3a65761c6852198286bb3e36af7d1e6235bac9e
salam aziz <azizsalam148@gmail.com>
2021-05-03 11:18:16 +0000

    hamburger close btn toggle added

--- src/app/components/main-app/app-header/index.jsx
+++ src/app/components/main-app/app-header/index.jsx
@@ -5,26 +5,34 @@ import CloseHamburger from "./../../../../assets/images/icon-close-menu.svg";
 import "./style.scss";
 
 export const AppHeader = () => {
+  useEffect(() => {
+    const hamburger = document.querySelector("#hamburger");
+    const navUl = document.querySelector("#nav-ul");
+    // console.log(hamburger.classList);
 
+    hamburger.addEventListener("click", () => {
+      if (navUl.classList.length === 1) {
+        hamburger.innerHTML =
+          '<img src= "' + CloseHamburger + ' " alt="hamburger icon"/> ';
+        navUl.classList.add("show");
+      } else {
+        hamburger.innerHTML =
+          '<img src= "' + Hamburger + ' " alt="hamburger icon"/> ';
+        navUl.classList.remove("show");
+      }
+      // console.log(navUl.classList);
+    });
+  });
 
-    useEffect(() => {
-        const hamburger = document.querySelector("#hamburger");
-        const navUl = document.querySelector("#nav-ul");
-        console.log(hamburger.classList.length);
-
-        hamburger.addEventListener('click', () => {
-            navUl.classList.toggle('show')
-        })
-    })
 
 
   return (
     <header className="hero">
       <div className="header-content">
         <nav id="nav" className="nav">
-        <span>
-          <img src={Logo} alt="crowdfund logo" />
-        </span>
+          <span>
+            <img src={Logo} alt="crowdfund logo" />
+          </span>
 
           <button className="menu" id="hamburger">
             <img src={Hamburger} alt="hamburger icon" />
